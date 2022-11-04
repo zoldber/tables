ce66f2d625c46b4030238d76c982c5b645ce16a5
stack-crash <zoldber@g.clemson.edu>
2022-11-03 23:34:44 -0400

    Remove helper files

--- src/row.hpp
+++
@@ -1,7 +0,0 @@
-//TODO: This should be the functional block of any table regardless of design or optimization
-//      The Row class should be passed a line from file read directly via cosntructor and it
-//      should automatically parse data in the fastest and safest way possible. Further, data
-//      should be organized for search optimization at the row level (hence tokens will be
-//      formatted into search trees withtin each row)
-
-class Row { };
